b2015b9d9964448468997df2bd3fd4778be19815
Jannes Bikker <jannesbikker@gmail.com>
2023-03-24 16:48:45 +0100

    refactor(PLayer.java): solved tasks for project 4

--- src/main/java/fourwins/Player.java
+++ src/main/java/fourwins/Player.java
@@ -1,4 +1,23 @@
 package fourwins;
 
-public class Player {
+import fourwins.Enums.Color;
+import fourwins.Exception.ColumnFullException;
+import fourwins.Exception.IllegalMoveException;
+
+import java.io.IOException;
+
+public abstract class Player {
+    private final Color color;
+    private final Bord bord;
+
+    public Player(Color color, Bord bord) {
+        this.color = color;
+        this.bord = bord;
+    }
+
+    protected void doDrop(int columnIndex) throws ColumnFullException, IllegalMoveException {
+        bord.dropToken(bord.getToken(color), columnIndex);
+    }
+
+    public abstract void doTurn() throws ColumnFullException, IllegalMoveException, IOException;
 }
