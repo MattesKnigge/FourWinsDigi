10eabe0f439c61e6c1e1e8fef494387c78907b16
Jannes Bikker <jannesbikker@gmail.com>
2023-03-24 14:22:43 +0100

    comments(Token.java): added comments to Token class

--- src/main/java/fourwins/Token.java
+++ src/main/java/fourwins/Token.java
@@ -2,21 +2,42 @@ package fourwins;
 
 import fourwins.Enums.Color;
 
+/**
+ * A token is a game object that can be placed on a board.
+ * It has a color, which can be red or yellow.
+ * The color of a token is used to determine the winner of the game.
+ */
+
 public class Token extends GameObject {
     private final Color color;
 
+    /**
+     * Constructor for Token
+     *
+     * @param color the color of the token
+     */
 
     public Token(Color color) {
         this.color = color;
     }
 
+    /**
+     * gets color
+     *
+     * @return the color of the token
+     */
     public Color getColor() {
         return color;
     }
 
+    /**
+     * toString method for Token
+     *
+     * @return the string representation of the token
+     */
     @Override
     public String toString() {
-        String s = "";
+        String s;
         s = switch (color) {
             case RED -> "X";
             case YELLOW -> "O";
