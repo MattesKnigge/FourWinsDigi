8ee02f19830402b645f66fbdbd129d9dfd1fc179
Jannes Bikker <jannesbikker@gmail.com>
2023-03-24 14:21:11 +0100

    comments(GameObject.java): added comments to GameObject class

--- src/main/java/fourwins/GameObject.java
+++ src/main/java/fourwins/GameObject.java
@@ -1,5 +1,10 @@
 package fourwins;
 
+/**
+ * The base class for all game objects.
+ * It contains a toString method, which returns a string representation of the object.
+ * The string representation of an object is used to display the object in the CLI.
+ */
 public abstract class GameObject {
     public abstract String toString();
 }
